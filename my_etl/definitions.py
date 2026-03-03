1e96ce7970fe5341e96c10a3f11faa93afde942a
innovation-hard <jorge.aguirre@outlook.com.ar>
2026-03-03 00:27:08 -0300

    Refactor definitions.py: remove raw_assets import and add cablemodem_json_sensor to sensors

--- my_etl/definitions.py
+++ my_etl/definitions.py
@@ -1,11 +1,12 @@
 from dagster import Definitions, load_assets_from_modules
 # Importación absoluta basada en la raíz del proyecto
-# import my_etl.assets.raw_assets as raw_assets
 import my_etl.assets.staging_assets as staging_assets
 import my_etl.assets.business_assets as business_assets
+from .sensors.file_sensors import cablemodem_json_sensor
 
 all_assets = load_assets_from_modules([staging_assets, business_assets])
 
 defs = Definitions(
     assets=all_assets,
+    sensors=[cablemodem_json_sensor],
 )
